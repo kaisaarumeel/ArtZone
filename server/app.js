dffb6df6ae2e7e30589d549759e2657ede76db20
alesaf <gussafala@student.gu.se>
2023-09-10 20:09:37 +0200

    Modified port to a const

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ mongoose.connect(mongoURI).catch(function(err) {
 });
 
 const users=require("./controllers/users")
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
 
