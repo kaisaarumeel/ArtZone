da9bbf2d5367427338361cfc5f0a2249c34a0b5f
arumeel <kaisa.arumeel@gmail.com>
2023-09-11 11:19:00 +0200

    Created get listing by page endpoint

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -214,4 +214,33 @@ router.patch("/users/:email/listings/:name", async function(req, res){
     }
 })    
 
-//GET /listings/page/:page  - Gets all listing in the system paginated.
+// GET /listings/page/:page - Gets all listings in the system paginated
+    router.get('/listings/page/:page', async (req, res) => {
+        try {
+            const page = parseInt(req.params.page);
+            const perPage = 50; 
+
+            // Calculate the skip value to skip listings on previous pages
+            const skip = (page - 1) * perPage;
+
+            // Query the database to retrieve listings for the specified page
+            const listings = await ListingSchema.find()
+                .skip(skip)
+                .limit(perPage);
+
+            // Determine the total number of listings in the system
+            const totalListings = await ListingSchema.countDocuments();
+
+            // Calculate the total number of pages based on the number of listings per page
+            const totalPages = Math.ceil(totalListings / perPage);
+
+            return res.status(200).json({
+                listings,
+                page,
+                totalPages,
+            });
+        } catch (error) {
+            console.error("Errors were found");
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    });
