5022e629acc883234f5e79f94b9f447d64783a43
arumeel <kaisa.arumeel@gmail.com>
2023-09-21 16:50:44 +0200

    Fix padding on about us

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home-page">
-    <h1 class="header pb-1 text-center mb-5">
+    <h1 class="header pb-1 text-center">
       <span class="bold-text">Discover</span> and <span class="bold-text">sell</span> <br> your <span class="bold-text">artwork</span>.
     </h1>
     <ArtCarouselVue></ArtCarouselVue>
     <b-row class="about-us mt-4 align-items-center" id="aboutUsSection">
       <b-col cols="12" lg="4" md="6"><img src="images/wave.jpeg" alt="Painting" class="painting p-5"></b-col>
-      <b-col cols="12" lg="8" md="6"><h2 class="pt-3 pr-4 text-right">About us</h2><p class="p-3">At Artzone, we believe that art is more than just paint on canvas or sculptures in a gallery; it's a powerful form of expression that connects individuals and cultures worldwide. Our mission is to create a vibrant and inclusive online community where artists of all backgrounds can showcase their creativity, share their stories, and find new audiences for their work. Whether you're a seasoned artist or a passionate art enthusiast, ArtZone provides a platform for you to explore, appreciate, and acquire unique pieces of art.
+      <b-col class="text-container pr-5 pt-5" cols="12" lg="8" md="6"><h2 class="text-right">About us</h2><p>At Artzone, we believe that art is more than just paint on canvas or sculptures in a gallery; it's a powerful form of expression that connects individuals and cultures worldwide. Our mission is to create a vibrant and inclusive online community where artists of all backgrounds can showcase their creativity, share their stories, and find new audiences for their work. Whether you're a seasoned artist or a passionate art enthusiast, ArtZone provides a platform for you to explore, appreciate, and acquire unique pieces of art.
         <br>
         <br>
         We have developed this platfrom with love and care. Here you can sell your own art or artwork made by another author. The platfrom offers a wide range of artwork, starting with paintings and drawings and ending with sculptures or graphical design. Join us on this creative journey, and together, let's celebrate the beauty and diversity of the art world. </p></b-col>
@@ -31,7 +31,7 @@ h1 {
   color: #606C5D;
 }
 .header {
-  font-size: 3rem;
+  font-size: 36px;
 }
 
 .bold-text {
@@ -52,17 +52,19 @@ h2 {
   color: #F7E6C4;
 }
 @media (max-width: 768px){
-  .header {
-    font-size: 36px;
-  }
-}
-@media (max-width: 768px){
   p {
     padding-top: 1rem;
   }
 
   .header {
     padding-top: 3rem;
+    font-size: 36px;
   }
+
+  .text-container {
+    padding: 3rem;
+    padding-top: 0 !important;
+  }
+
 }
 </style>
