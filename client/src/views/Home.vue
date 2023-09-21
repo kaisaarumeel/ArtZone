3ecc86074ff663cee4fa3d4b0fd058f64d5063ba
alesaf <gussafala@student.gu.se>
2023-09-21 11:59:58 +0200

    Removed rules that already exist

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home-page">
-    <h1 class="header">
+    <h1 class="header pb-1 text-center">
       <span class="bold-text">Discover</span> and <span class="bold-text">sell</span> <br> your <span class="bold-text">artwork</span>.
     </h1>
     <ArtCarouselVue></ArtCarouselVue>
-    <b-row class="about-us" id="aboutUsSection">
+    <b-row class="about-us mt-4 align-items-center" id="aboutUsSection">
       <b-col cols="12" lg="4" md="6"><img src="images/wave.jpeg" alt="Painting" class="painting p-5"></b-col>
-      <b-col cols="12" lg="8" md="6"><h2>About us</h2><p>At Artzone, we believe that art is more than just paint on canvas or sculptures in a gallery; it's a powerful form of expression that connects individuals and cultures worldwide. Our mission is to create a vibrant and inclusive online community where artists of all backgrounds can showcase their creativity, share their stories, and find new audiences for their work. Whether you're a seasoned artist or a passionate art enthusiast, ArtZone provides a platform for you to explore, appreciate, and acquire unique pieces of art.
+      <b-col cols="12" lg="8" md="6"><h2 class="pt-3 pr-4 text-right">About us</h2><p class="p-3">At Artzone, we believe that art is more than just paint on canvas or sculptures in a gallery; it's a powerful form of expression that connects individuals and cultures worldwide. Our mission is to create a vibrant and inclusive online community where artists of all backgrounds can showcase their creativity, share their stories, and find new audiences for their work. Whether you're a seasoned artist or a passionate art enthusiast, ArtZone provides a platform for you to explore, appreciate, and acquire unique pieces of art.
         <br>
         <br>
         We have developed this platfrom with love and care. Here you can sell your own art or artwork made by another author. The platfrom offers a wide range of artwork, starting with paintings and drawings and ending with sculptures or graphical design. Join us on this creative journey, and together, let's celebrate the beauty and diversity of the art world. </p></b-col>
@@ -32,19 +32,8 @@ h1 {
 }
 .header {
   font-size: 36px;
-  text-align: center;
-  justify-content: center;
-  padding-bottom: 1rem;
 }
 
-.about-us {
-  align-items: center;
-}
-@media (max-width: 768px){
-  .header {
-    font-size: 36px;
-  }
-}
 .bold-text {
   font-weight:900;
 }
@@ -52,26 +41,21 @@ h1 {
 .painting {
   width: 100%;
   height: auto;
-  padding-bottom: 0;
-
 }
 .about-us {
   background: #606C5D;
-  margin-top: 2rem;
 }
-
 p {
   color: #F7E6C4;
-  padding: 3rem;
 }
-
 h2 {
   color: #F7E6C4;
-  text-align: end;
-  padding-right: 3rem;
-  padding-top: 1rem;
 }
-
+@media (max-width: 768px){
+  .header {
+    font-size: 36px;
+  }
+}
 @media (max-width: 768px){
   p {
     padding-top: 1rem;
@@ -81,5 +65,4 @@ h2 {
     padding-top: 3rem;
   }
 }
-
 </style>
