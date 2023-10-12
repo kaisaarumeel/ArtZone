32aeda443b895067543a44b21c23345e9b007c63
arumeel <kaisa.arumeel@gmail.com>
2023-10-12 13:01:38 +0200

    add padding on home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-page">
-    <h1 class="header pb-5 text-center">
+    <h1 class="header pb-5 pt-5 text-center">
       <span class="bold-text">Discover</span> and <span class="bold-text">sell</span> <br> your <span class="bold-text">artwork</span>.
     </h1>
     <ArtCarouselVue></ArtCarouselVue>
